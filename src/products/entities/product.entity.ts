87c61e0954851c5f491003fb8ce4cf65ae320d28
Oscar Aguilar <aguilop.oscar@gmail.com>
2022-10-17 22:21:15 -0500

    Modulo de productos

---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,18 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text', {
+    unique: true,
+  })
+  title: string;
+
+  @Column('text', { nullable: true })
+  description: string;
+
+  @Column('numeric', { default: 0 })
+  price: number;
+}
